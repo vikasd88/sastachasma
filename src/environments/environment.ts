5ad992d49cd6a56bb82438d577d30cc1bf797abb
Vikas <65715532+vikasd88@users.noreply.github.com>
2025-11-11 22:28:05 +0530

    3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://sastachasma-backend-1.onrender.com/api/products/app',//'http://localhost:8081/api', // Your Spring Boot backend URL
+  apiUrl: 'https://sastachasma-backend-1.onrender.com/api',//'http://localhost:8081/api', // Your Spring Boot backend URL
   defaultUserId: 1 // Default user ID for development
 };
