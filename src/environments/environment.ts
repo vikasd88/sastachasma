f1284d6a4c008307f4d92a3814474aa27af494a5
Vikas <65715532+vikasd88@users.noreply.github.com>
2025-11-13 17:20:28 +0530

    deplyment server ip update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8085/api', // Consolidated and corrected to API Gateway's port and base path
+  apiUrl: 'http://100.81.50.73:32000/api', // Consolidated and corrected to API Gateway's port and base path
   defaultUserId: "1" // Default user ID for development
 };
